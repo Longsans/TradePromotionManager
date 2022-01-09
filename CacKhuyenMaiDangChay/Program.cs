4b62caa849cd52cb14b69e2a1b1fe79396b9d7e7
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2022-01-09 23:58:45 +0700

    idk

---
+++ CacKhuyenMaiDangChay/Program.cs
@@ -0,0 +1,6 @@
+﻿using CacKhuyenMaiDangChay.Models;
+
+KhuyenMai km = new KhuyenMai("{\"thoiGianBatDau\":\"12/12/2021\",\"thoiGianKetThuc\":\"2/2/2022\",\"soLuongHoaDon\":12,\"soLuongMucTieu\":200,\"von\":10000,\"tongThanhTien\":100000,\"laiSuatMucTieu\":50000}");
+
+Console.WriteLine("[Cac Khuyen Mai Dang Chay]");
+Console.WriteLine(km.toJson());
