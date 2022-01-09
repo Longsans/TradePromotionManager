68eb734bc4502d350d3c901071b55246ec9c6899
Long <gameofnoobs17@gmail.com>
2022-01-09 18:52:43 +0700

    add message bus

---
+++ MessageBus/MessageBus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MessageBus
+{
+    public class MessageBus
+    {
+        public event Action<string>? MessageSent;
+        
+        public void SendMessage(string message)
+        {
+            MessageSent?.Invoke(message);
+        }
+    }
+}
