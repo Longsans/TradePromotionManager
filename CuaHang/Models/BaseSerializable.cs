b4318a1fd55cea47e0860efa8e4bb7b7f6428f09
Nguyen Pham <19521918@gm.uit.edu.vn>
2022-01-09 23:10:02 +0700

    f

---
+++ CuaHang/Models/BaseSerializable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CuaHang.Models
+{
+    [Serializable]
+    internal abstract class BaseSerializable
+    {
+        public BaseSerializable() { }
+
+        public string Encode()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static T? Decode<T>(string json) where T : BaseSerializable
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
